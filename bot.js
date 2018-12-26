9c7be9a76359b1f1dae14c31de8889a6c1933781
hxfumax <46162817+hxfumax@users.noreply.github.com>
2018-12-26 16:56:24 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("527031704209063936")
+setInterval(function() {
+channel.send(`فلوووس الامير مهمل اويليء يه يه يه يه يه`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
